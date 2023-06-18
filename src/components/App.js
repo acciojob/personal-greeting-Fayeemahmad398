7d3333da7142ffabfbfa8c6267bc13f80e5b1d18
Fayeemahmad398 <fayeemahmad0@gmail.com>
2023-06-18 15:48:13 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [name, setName] = useState("");
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
+      <h3>Enter your name:</h3>
+      <input
+        type="text"
+        onChange={(event) => {
+          setName(event.target.value);
+        }}
+      />
 
-export default App
+      {name && <p>Hello {name}!</p>}
+    </div>
+  );
+};
+
+export default App;
